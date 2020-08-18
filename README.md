44795aee267715e25aa92f371d44ca024781ef97
Trujillo <heber.trj.urt@gmail.com>
2020-08-18 21:22:15 +0200

    Generator and Discriminator classes were added

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-"# Generative-Adversarial-Networks" 
+# Generative-Adversarial-Networks
+
+This python package provides a general framework to generate small images (e.g. 28 x 28 pixels) using a generative adversarial network (GAN).
+
+We have been using the general theoretical background provided by Ian Goodfellow on his [Generative Adversarial Networks](https://arxiv.org/abs/1406.2661) research paper.
+
+## Intuition
+
+The idea behind GANs is that two networks, a generator  𝐺, and a discriminator  𝐷, will compete against each other. The generator makes "fake" data to pass to the discriminator. The discriminator also sees real training data and predicts if the data it's received is real or fake.
+
+The following diagram shows the general idea:
+
+![image](https://user-images.githubusercontent.com/28582065/90556171-bbc8d080-e198-11ea-88ba-9beb3ef8cef6.png)
---
+++ network/architecture.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 18 20:45:02 2020
+
+@author: HTRUJILLO
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Discriminator(nn.Module):
+
+    def __init__(self, input_size, hidden_dim, output_size):
+        super(Discriminator, self).__init__()
+        
+        self.fc1 = nn.Linear(input_size, hidden_dim*4)
+        self.fc2 = nn.Linear(hidden_dim*4, hidden_dim*2)
+        self.fc3 = nn.Linear(hidden_dim*2, hidden_dim)
+        
+        self.fc4 = nn.Linear(hidden_dim, output_size)
+        
+        
+        self.dropout = nn.Dropout(0.25)
+        
+    def forward(self, x):
+        
+        x = torch.flatten(x)
+        
+        x = F.leaky_relu(self.fc1(x), 0.2)
+        x = self.dropout(x)
+        
+        x = F.leaky_relu(self.fc2(x), 0.2)
+        x = self.dropout(x)
+        
+        x = F.leaky_relu(self.fc3(x), 0.2)
+        x = self.dropout(x)
+        
+        out = self.fc4(x)
+        
+        return out
+
+
+class Generator(nn.Module):
+
+    def __init__(self, input_size, hidden_dim, output_size):
+        super(Generator, self).__init__()
+        
+        self.fc1 = nn.Linear(input_size, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim*2)
+        self.fc3 = nn.Linear(hidden_dim*2, hidden_dim*4)
+        
+        self.fc4 = nn.Linear(hidden_dim*4, output_size)
+
+        self.dropout = nn.Dropout(0.25)
+        
+    def forward(self, x):
+        
+        x = F.leaky_relu(self.fc1(x), 0.2)
+        x = self.dropout(x)
+        
+        x = F.leaky_relu(self.fc2(x), 0.2)
+        x = self.dropout(x)
+        
+        x = F.leaky_relu(self.fc3(x), 0.2)
+        x = self.dropout(x)
+        
+        out = F.tanh(self.fc4(x))
+        
+        return out
